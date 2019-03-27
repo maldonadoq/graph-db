4eb63dac7fbb04610e83014d22a22846e8aa7286
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-03-27 11:39:10 -0500

    Load Balancer Server!

---
+++ balancer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "src/load-balancer.h"
+
+int main(int argc, char const *argv[]){
+	
+	int port = 5555;
+	int n = 5;
+	int q = 3;
+
+	TLoadBalancer *lb = new TLoadBalancer(q);
+	lb->onConnect(port);
+	lb->onListening();
+
+	delete lb;
+	return 0;
+}
