493f143a5dcd6cecc89bb6333f9f55fdb44c7a1c
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-04-14 20:03:27 -0500

    query main, similar to client but different port!

---
+++ query.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "src/client.h"
+
+int main(int argc, char const *argv[]){
+		
+	std::string ip = "192.168.1.41";
+	int port = 7777;
+
+	TClient *cl = new TClient();
+	cl->Connect(ip, port);
+	cl->Talking();
+
+	delete cl;
+	return 0;
+}
